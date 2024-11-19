2386168f2b784e774459afcdacbc60da11004994
Akzert <Nikitabyk163@gmail.com>
2024-11-19 10:37:29 +0400

    Add files via upload

---
+++ Scripts/CalculatorLogic.cs
@@ -0,0 +1,21 @@
+﻿public class CalculatorLogic
+{
+    // Выполняет расчет на основе двух операндов и операции
+    public float Calculate(float operand1, float operand2, string operation)
+    {
+        switch (operation)
+        {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                if (operand2 == 0) throw new System.DivideByZeroException("Cannot divide by zero");
+                return operand1 / operand2;
+            default:
+                throw new System.InvalidOperationException("Invalid operation");
+        }
+    }
+}
